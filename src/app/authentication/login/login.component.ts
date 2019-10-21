36fc2fcc7620ba78f8371efc5606d105a437ec01
Monika Chauhan <mchauhan@optiproerp.com>
2019-10-21 17:24:46 +0530

    changes in dashboard

--- src/app/authentication/login/login.component.ts
+++ src/app/authentication/login/login.component.ts
@@ -5,58 +5,45 @@ import { LoginService } from '../../service/login.service';
 import { MessageService } from '../../common/message.service';
 import { TranslateService, LangChangeEvent } from '@ngx-translate/core';
 import { HttpClient, HttpErrorResponse } from '@angular/common/http';
-import { AuthService } from './../../auth.service';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
-
 export class LoginComponent {    
     
   model : any={};    
   successmsg: any;  
   selectedItem: string = ""; 
-  errorMessage:string;   
-  arrConfigData:any=[];
-  
+  errorMessage:string;    
   constructor(private router:Router,private LoginService:LoginService,private MessageService:MessageService,
-    private translate: TranslateService, private httpClientSer: HttpClient,private auth: AuthService) { 
+    private translate: TranslateService, private httpClientSer: HttpClient) { 
       translate.use(localStorage.getItem('applang'));
         translate.onLangChange.subscribe((event: LangChangeEvent) => {
             this.selectedItem = translate.instant("Login_Username"); 
         }); 
     }    
-    ngOnInit() {   
-      this.httpClientSer.get('./assets/config.json').subscribe(
-        data => {
-          this.arrConfigData = data as string[];
-          window.localStorage.setItem('arrConfigData', this.arrConfigData.service_url);
-        },
-         (err: HttpErrorResponse) => {
-           console.log(err.message);
-        });
+    ngOnInit() {     
     sessionStorage.removeItem('UserName');    
     sessionStorage.clear();    
   }   
 
-  login(){  
+  login(){     
     this.selectedItem = this.translate.instant("Login_Username");
     this.LoginService.Login(this.model).subscribe(    
       data => {    
         //if(data.Status=="Success") 
         if(data != null)   
         {       
-          this.successmsg = 'token - ' + data[0].access_token;  
-          localStorage.setItem('access_token', data[0].access_token);       
-          localStorage.setItem('token_type', data[0].token_type); 
-          localStorage.setItem('expires_in', data[0].expires_in); 
-          this.auth.sendToken(data[0].access_token)
+          this.successmsg = 'token - ' + data.access_token;  
+          localStorage.setItem('access_token', data.access_token);       
+          localStorage.setItem('token_type', data.token_type); 
+          localStorage.setItem('expires_in', data.expires_in); 
           // this.router.navigate(['/main']); 
           this.AdminLoginLog();     
         }    
         else{ 
-          this.MessageService.errormessage(this.translate.instant('UPInvaild'));
+          this.MessageService.errormessage("UserName or Password is invalid");
           //this.errorMessage = data.Message;    
         }    
       },    
@@ -68,13 +55,12 @@ export class LoginComponent {
     this.LoginService.AdminLoginLog(this.model).subscribe(    
       data => {     
         //if(data.Status=="Success") 
-        if(data == "True")   
+        if(data.data == "True")   
         {     
           this.router.navigate(['/main']);       
         }    
         else{ 
-          this.router.navigate(['/main']);   
-          this.MessageService.errormessage(this.translate.instant('UPInvaild'));
+          this.MessageService.errormessage("UserName or Password is invalid");
           //this.errorMessage = data.Message;    
         }    
       },    
--- src/app/service/login.service.ts
+++ src/app/service/login.service.ts
@@ -12,26 +12,31 @@ export class LoginService {
   token : string;  
   header : any;
   constructor(private http : HttpClient) {
-    this.Url= localStorage.getItem('arrConfigData');
+    this.Url = 'http://localhost:21534/api/login/AdminLogin/';  
+    const headerSettings: {[name: string]: string | string[]; } = {};  
+    this.header = new HttpHeaders(headerSettings);  
    }
 
    Login(model : any){ 
+      
      var jObject = { LoginDetail: JSON.stringify([{ LoginId: model.UserName, LoginPassword: model.Password }]) } //bug no 14986 Tamanna Feb 
       
-    
-   //return this.http.post<any>(this.Url+'UserLogin',model,{ headers: this.header}); 
-   return this.http.post<any>(this.Url+'/api/login/AdminLogin/',jObject,{ headers: this.header});
-   
+     var a =this.Url+'UserLogin';  
+   //return this.http.post<any>(this.Url+'UserLogin',model,{ headers: this.header});
+   return this.http.post<any>(this.Url,jObject,{ headers: this.header});
 }
 AdminLoginLog(model : any)
 {
      
   var today = new Date();
   var date = today.toLocaleString();
+   
+
+  this.Url = 'http://localhost:21534/api/login/AdminLoginLog/';  
       const headerSettings: {[name: string]: string | string[]; } = {};  
       this.header = new HttpHeaders(headerSettings); 
-      var jObject = { LoginDetail: JSON.stringify([{"adminUser":model.UserName,"adminLogDateTime": date }])}
+      var jObject = { LoginDetail: JSON.stringify([{"adminUser":model.UserName,"adminLogDateTime": date }])} //bug no 14986 Tamanna Feb 
      
-      return this.http.post<any>(this.Url+'/api/login/AdminLoginLog',jObject,{ headers: this.header});
+    return this.http.post<any>(this.Url,jObject,{ headers: this.header});
 }
 }
