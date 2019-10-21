45831b324c40fb90675e423f382da9faa93af4e9
Sheetal Parihar <sparihar@optiproerp.com>
2019-10-21 14:38:31 +0530

    changes in dashboard

--- src/app/main/connected-users/connected-users.component.ts
+++ src/app/main/connected-users/connected-users.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { products } from 'src/app/dummyData/data';
 import { GridComponent } from '@progress/kendo-angular-grid';
+import { ConnectedusersService } from 'src/app/service/Connectedusers.service';
+import { MessageService } from '../../common/message.service';
 
 @Component({
   selector: 'app-connected-users',
@@ -15,12 +16,16 @@ export class ConnectedUsersComponent implements OnInit {
   // public paginationPageSizes = true;
   // public paginationInfoPreviousNext = true;
   public gridData: any[];
+  public ddlProductList: any[];
+  public PlaceHolder = { ProductId: 'Select Product..'};
+  public ProductName: string = 'Select Product..';
 
-  constructor() { }
+  constructor(private ConnectedUserServ: ConnectedusersService, private MessageService:MessageService) { }
 
   ngOnInit() {
-    // this.gridData = products;
+        
     // this.isMobile();
+    this.getProductList();
   }
 
   onFilterChange(checkBox:any,grid:GridComponent){
@@ -41,4 +46,66 @@ export class ConnectedUsersComponent implements OnInit {
   //     this.paginationButtonCount = 3;                 
   //   }
   // }
+
+  getProductList(){
+
+    this.ConnectedUserServ.getProductList().subscribe(
+      data => {
+        this.ddlProductList = data; 
+        console.log(data);
+      },    
+      error => {  
+        this.MessageService.errormessage(error.message);
+      }); 
+  } 
+
+  productChange($event){
+    this.ProductName = $event.ProductId;
+  }
+
+  onClickDisplay(){    
+
+    let Product = '';
+    let Controller = ''
+
+    switch (this.ProductName)   
+    { 
+      case 'ATD': 
+             Product = 'OptiWMS'; 
+             break;
+
+      case 'CNF': 
+            Product = 'optiproconfigurator'; Controller = 'Base';
+            break;
+
+      case 'CVP': 
+            Product = 'OptiWMS';
+            break;
+
+      case 'WMS': 
+             Product = 'OptiPROWMS'; Controller = 'Login';
+             break;
+             
+
+      default:
+            Product = 'OptiAdmin';
+            break;
+
+    }
+
+    this.ConnectedUserServ.getConnectedUserData(Product,Controller).subscribe(
+      data => {
+        console.log(data);
+        if(data != undefined && data != null){
+          this.gridData = data.LoggedUserData;
+        }     
+        else{
+          this.MessageService.errormessage("No record found");
+       }
+      },    
+      error => {  
+        this.MessageService.errormessage(error.message);
+    });
+  }
+ 
 }
